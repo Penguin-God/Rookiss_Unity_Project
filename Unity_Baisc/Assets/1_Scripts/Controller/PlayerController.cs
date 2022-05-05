bdf0ec3db27a61cb40d128fd0da72f5616e75cdc
Penguin-God <gkswh4860@naver.com>
2022-05-05 19:27:39 +0900

    animation 기초 ~ State 패턴

--- Unity_Baisc/Assets/1_Scripts/Controller/PlayerController.cs
+++ Unity_Baisc/Assets/1_Scripts/Controller/PlayerController.cs
@@ -2,77 +2,76 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum PlayerState
+{
+    Idle,
+    Moveing,
+    Jumping,
+    Falling,
+}
+
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float speed;
+    //[SerializeField] bool _isMoveToDestination;
 
-    [SerializeField] bool _isMoveToDestination;
+    PlayerState _state = PlayerState.Idle;
+    Animator _anim;
     Vector3 _destination;
 
     void Start()
     {
-        Managers.Input.OnKeyInput -= OnKeyboard;
-        Managers.Input.OnKeyInput += OnKeyboard;
+        _anim = GetComponent<Animator>();
         Managers.Input.OnMouseInput -= MouseDownAction;
         Managers.Input.OnMouseInput += MouseDownAction;
     }
 
+    [SerializeField] float wait_run_ratio;
     void Update()
     {
-        MoveToDestination();
+        switch (_state)
+        {   
+            case PlayerState.Idle: UpdateIdle(); break;
+            case PlayerState.Moveing: UpdateMove(); break;
+            default: break;
+        }
     }
 
-    void OnKeyboard()
+    private void UpdateIdle()
     {
-        // TODO : _isMoveToDestination = false; 부분 리펙토링
-        if (Input.GetKey(KeyCode.W))
-        {
-            // Quaternion.LookRotation(Vector3) : 인자값에 들어간 Vector3좌표를 바라보는 쿼터니언 return
-            // transform.Translate()는 이동 방향이 자기 기준이라 이렇게 방향을 바꿔줄 거면 그냥 transform.position에 직빵으로 더하는게 나음
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.2f);
-            transform.position += Vector3.forward * Time.deltaTime * speed;
-            _isMoveToDestination = false;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.2f);
-            transform.position += Vector3.back * Time.deltaTime * speed;
-            _isMoveToDestination = false;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
-            transform.position += Vector3.right * Time.deltaTime * speed;
-            _isMoveToDestination = false;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.2f);
-            transform.position += Vector3.left * Time.deltaTime * speed;
-            _isMoveToDestination = false;
-        }
+        wait_run_ratio = Mathf.Lerp(wait_run_ratio, 0, 10.0f * Time.deltaTime);
+        _anim.SetFloat("wait_run_ratio", wait_run_ratio);
+        _anim.Play("WAIT_RUN");
     }
+
+    private void UpdateMove()
+    {
+        MoveToDestination();
+        wait_run_ratio = Mathf.Lerp(wait_run_ratio, 1, 10.0f * Time.deltaTime);
+        _anim.SetFloat("wait_run_ratio", wait_run_ratio);
+        _anim.Play("WAIT_RUN");
+    }
+
+
 
     void MouseDownAction(Define.MouseEvent mouseEvent)
     {
-        if (mouseEvent != Define.MouseEvent.Down) return;
-
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if(Physics.Raycast(ray, out RaycastHit hitInfo, 100, LayerMask.GetMask("Plane")))
         {
             _destination = hitInfo.point;
-            _isMoveToDestination = true;
+            _state = PlayerState.Moveing;
         }
     }
 
     void MoveToDestination()
     {
-        if (_isMoveToDestination)
+        if (_state == PlayerState.Moveing)
         {
             Vector3 dir = _destination - transform.position;
 
             if (dir.magnitude < 0.001f) 
-                _isMoveToDestination = false;
+                _state = PlayerState.Idle;
             else
             {
                 float moveDistance = Mathf.Clamp(speed * Time.deltaTime, 0, dir.magnitude);
@@ -81,4 +80,31 @@ public class PlayerController : MonoBehaviour
             }
         }
     }
+
+    // 이제는 안쓰지만 아까우니까 남겨둔 코드
+    //void OnKeyboard()
+    //{
+    //    if (Input.GetKey(KeyCode.W))
+    //    {
+    //        // Quaternion.LookRotation(Vector3) : 인자값에 들어간 Vector3좌표를 바라보는 쿼터니언 return
+    //        // transform.Translate()는 이동 방향이 자기 기준이라 이렇게 방향을 바꿔줄 거면 그냥 transform.position에 직빵으로 더하는게 나음
+    //        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.2f);
+    //        transform.position += Vector3.forward * Time.deltaTime * speed;
+    //    }
+    //    if (Input.GetKey(KeyCode.S))
+    //    {
+    //        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.2f);
+    //        transform.position += Vector3.back * Time.deltaTime * speed;
+    //    }
+    //    if (Input.GetKey(KeyCode.D))
+    //    {
+    //        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
+    //        transform.position += Vector3.right * Time.deltaTime * speed;
+    //    }
+    //    if (Input.GetKey(KeyCode.A))
+    //    {
+    //        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.2f);
+    //        transform.position += Vector3.left * Time.deltaTime * speed;
+    //    }
+    //}
 }
